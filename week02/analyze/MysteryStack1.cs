c50146267b8f14af901b72e2b3d489c3f399237c
MBarker2BYU <mbarker2@byupathway.edu>
2025-09-02 22:36:18 -0400

    Added Comments to the stack code

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,13 +1,28 @@
 ﻿public static class MysteryStack1 {
-    public static string Run(string text) {
+
+    /// <summary>
+    /// Reverses the input string using a stack
+    /// </summary>
+    /// <param name="text">The input string to reverse</param>
+    /// <returns>The reversed string</returns>
+    public static string Run(string text)
+    {
+
+        // Create a stack to hold the characters
         var stack = new Stack<char>();
+
+        // Push each character onto the stack
         foreach (var letter in text)
             stack.Push(letter);
 
+        // Initialize the result string
         var result = "";
+
+        // Pop each character from the stack and append to the result
         while (stack.Count > 0)
             result += stack.Pop();
 
+        // Return the reversed string
         return result;
     }
 }
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,49 +1,94 @@
 ﻿public static class MysteryStack2 {
-    private static bool IsFloat(string text) {
+
+    /// <summary>
+    /// Checks if the input string is a valid float or can be converted to one.
+    /// </summary>
+    /// <param name="text">The input string to check</param>
+    /// <returns>True if the string is a valid float, otherwise false</returns>
+    private static bool IsFloat(string text)
+    {
+        //Try to convert to a float
         return float.TryParse(text, out _);
     }
 
-    public static float Run(string text) {
+    // Implement an RPN (Reverse Polish Notation) calculator
+    // The input is a string containing space-separated tokens
+    // The output is a single float value
+    /// <param name="text">The input string to evaluate</param>
+    /// <returns>The result of the RPN evaluation</returns>
+    public static float Run(string text)
+    {
+        // Create a stack to hold the float values
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
+
+        // Split the input text into tokens and process each one
+        foreach (var item in text.Split(' '))
+        {
+            // Check if the item is an operator
+            if (item == "+" || item == "-" || item == "*" || item == "/")
+            {
+                // Ensure there are at least two operands in the stack
                 if (stack.Count < 2)
                     throw new ApplicationException("Invalid Case 1!");
 
+                // Pop the top operand from the stack
                 var op2 = stack.Pop();
+                // Pop the next operand from the stack
                 var op1 = stack.Pop();
+
+                // Result variable
                 float res;
-                if (item == "+") {
+
+                // Perform the operation based on the operator
+                if (item == "+")
+                {
+                    // Add the two operands
                     res = op1 + op2;
                 }
-                else if (item == "-") {
+                else if (item == "-")
+                {
+                    // Subtract the two operands
                     res = op1 - op2;
                 }
-                else if (item == "*") {
+                else if (item == "*")
+                {
+                    // Multiply the two operands
                     res = op1 * op2;
                 }
-                else {
+                else
+                {
+                    // Check for division by zero
                     if (op2 == 0)
                         throw new ApplicationException("Invalid Case 2!");
 
+                    // Divide the two operands
                     res = op1 / op2;
                 }
 
+                // Push the result back onto the stack
                 stack.Push(res);
             }
-            else if (IsFloat(item)) {
+            // Check if the item is a float
+            else if (IsFloat(item))
+            {
+                // Parse the item as a float and push it onto the stack     
                 stack.Push(float.Parse(item));
             }
-            else if (item == "") {
+            // Check if the item is an empty string
+            else if (item == "")
+            {
             }
-            else {
+            else
+            {
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
 
+        // Ensure there is exactly one result on the stack
         if (stack.Count != 1)
             throw new ApplicationException("Invalid Case 4!");
 
+        // Return the final result
         return stack.Pop();
     }
 }
